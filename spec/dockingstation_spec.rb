b510ca7f3e604aa374e46f6e1390c44585891808
anaalta <suciuanamaria@yahoo.com>
2017-04-11 13:07:54 +0200

    added challenge 10

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -2,5 +2,13 @@ require 'dockingstation'
 describe DockingStation do
   it 'responds_to release_bike' do
   is_expected.to respond_to(:release_bike)
+  end
+  it 'responds_to working?' do
+  is_expected.to respond_to(:working?)
+  end
 end
+
+describe :release_bike do
+it { expect :release_bike == Bike.new }
+it {expect Bike.new.respond_to? :working?}
 end
