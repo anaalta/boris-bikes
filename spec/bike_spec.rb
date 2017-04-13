389deebdc9798145580760214880cc0d382c4162
anaalta <suciuanamaria@yahoo.com>
2017-04-13 12:44:47 +0200

    ex 18 partially

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,13 +1,5 @@
 require 'Bike'
 
 describe Bike do
-  it { is_expected.to respond_to :working }
-  it "checks if the bike is working (default true)" do
-    expect(subject.working).to eq true
-  end
-  it "checks if the default for working can be changed" do
-    expect(subject.working=false).to eq false
-  end
-
-
+  it { is_expected.to respond_to :working? }
 end
